424bebd44abc46dea35be77f41c87e8ef0252fda
Chris Reid <christopherjreid@outlook.com>
2022-08-01 07:51:05 -0400

    Source workspace in action

--- rclrs_tests/src/graph_tests.rs
+++ rclrs_tests/src/graph_tests.rs
@@ -11,32 +11,32 @@ fn foo() -> Result<(), RclrsError> {
     let _node_1_empty_publisher =
         node1.create_publisher::<msg::Empty>("graph_test_topic_1", QOS_PROFILE_SYSTEM_DEFAULT)?;
     let _node_1_basic_types_publisher = node1
-        .create_publisher::<msg::BasicTypes>("graph_test_topic_1", QOS_PROFILE_SYSTEM_DEFAULT)?;
+        .create_publisher::<msg::BasicTypes>("graph_test_topic_2", QOS_PROFILE_SYSTEM_DEFAULT)?;
     let _node_1_defaults_subscription = node1.create_subscription::<msg::Defaults, _>(
-        "graph_test_topic_2",
+        "graph_test_topic_3",
         QOS_PROFILE_SYSTEM_DEFAULT,
         |_msg| {},
     )?;
     let _node_1_empty_service =
-        node1.create_service::<srv::Empty, _>("graph_test_topic_3", |_request_id, _srv| {
+        node1.create_service::<srv::Empty, _>("graph_test_topic_4", |_request_id, _srv| {
             srv::Empty_Response {
                 structure_needs_at_least_one_member: 0,
             }
         })?;
 
     let _node_2_default_publisher = node2
-        .create_publisher::<msg::Defaults>("graph_test_topic_2", QOS_PROFILE_SYSTEM_DEFAULT)?;
+        .create_publisher::<msg::Defaults>("graph_test_topic_3", QOS_PROFILE_SYSTEM_DEFAULT)?;
     let _node_2_empty_subscription = node2.create_subscription::<msg::Empty, _>(
         "graph_test_topic_1",
         QOS_PROFILE_SYSTEM_DEFAULT,
         |_msg| {},
     )?;
     let _node_2_basic_types_subscription = node2.create_subscription::<msg::BasicTypes, _>(
-        "graph_test_topic_1",
+        "graph_test_topic_2",
         QOS_PROFILE_SYSTEM_DEFAULT,
         |_msg| {},
     )?;
-    let _node_2_empty_client = node2.create_client::<srv::Empty>("graph_test_topic_3")?;
+    let _node_2_empty_client = node2.create_client::<srv::Empty>("graph_test_topic_4")?;
 
     check_publishers(&node1, &node2)?;
     check_subscriptions(&node1, &node2)?;
@@ -48,39 +48,45 @@ fn foo() -> Result<(), RclrsError> {
 }
 
 fn check_publishers(node1: &Node, node2: &Node) -> Result<(), RclrsError> {
+    assert_eq!(node1.count_publishers("/graph_test_topic_1")?, 1);
+    assert_eq!(node1.count_publishers("/graph_test_topic_2")?, 1);
     let node_1_publisher_names_and_types =
         node1.get_publisher_names_and_types_by_node("graph_test_node_1", "/")?;
 
     let types = node_1_publisher_names_and_types
         .get("/graph_test_topic_1")
         .unwrap();
-
-    assert_eq!(node1.count_publishers("/graph_test_topic_1")?, 2);
-    assert!(types.contains(&"test_msgs/msg/BasicTypes".to_string()));
     assert!(types.contains(&"test_msgs/msg/Empty".to_string()));
+
+    let types = node_1_publisher_names_and_types
+        .get("/graph_test_topic_2")
+        .unwrap();
+    assert!(types.contains(&"test_msgs/msg/BasicTypes".to_string()));
 
     let node_2_publisher_names_and_types =
         node2.get_publisher_names_and_types_by_node("graph_test_node_2", "/")?;
 
     let types = node_2_publisher_names_and_types
-        .get("/graph_test_topic_2")
+        .get("/graph_test_topic_3")
         .unwrap();
-    assert_eq!(node2.count_publishers("/graph_test_topic_2")?, 1);
+    assert_eq!(node2.count_publishers("/graph_test_topic_3")?, 1);
     assert!(types.contains(&"test_msgs/msg/Defaults".to_string()));
 
     Ok(())
 }
 
 fn check_subscriptions(node1: &Node, node2: &Node) -> Result<(), RclrsError> {
+    assert_eq!(node1.count_subscriptions("/graph_test_topic_3")?, 1);
     let node_1_subscription_names_and_types =
         node1.get_subscription_names_and_types_by_node("graph_test_node_1", "/")?;
 
     let types = node_1_subscription_names_and_types
-        .get("/graph_test_topic_2")
+        .get("/graph_test_topic_3")
         .unwrap();
-
-    assert_eq!(node1.count_subscriptions("/graph_test_topic_2")?, 1);
     assert!(types.contains(&"test_msgs/msg/Defaults".to_string()));
+
+    assert_eq!(node2.count_subscriptions("/graph_test_topic_1")?, 1);
+    assert_eq!(node2.count_subscriptions("/graph_test_topic_2")?, 1);
 
     let node_2_subscription_names_and_types =
         node2.get_subscription_names_and_types_by_node("graph_test_node_2", "/")?;
@@ -88,10 +94,12 @@ fn check_subscriptions(node1: &Node, node2: &Node) -> Result<(), RclrsError> {
     let types = node_2_subscription_names_and_types
         .get("/graph_test_topic_1")
         .unwrap();
-
-    assert_eq!(node2.count_subscriptions("/graph_test_topic_1")?, 2);
-    assert!(types.contains(&"test_msgs/msg/BasicTypes".to_string()));
     assert!(types.contains(&"test_msgs/msg/Empty".to_string()));
+
+    let types = node_2_subscription_names_and_types
+        .get("/graph_test_topic_2")
+        .unwrap();
+    assert!(types.contains(&"test_msgs/msg/BasicTypes".to_string()));
 
     Ok(())
 }
@@ -100,12 +108,11 @@ fn check_topic_names_and_types(node: &Node) -> Result<(), RclrsError> {
     let topic_names_and_types = node.get_topic_names_and_types()?;
 
     let types = topic_names_and_types.get("/graph_test_topic_1").unwrap();
-
-    assert!(types.contains(&"test_msgs/msg/BasicTypes".to_string()));
     assert!(types.contains(&"test_msgs/msg/Empty".to_string()));
-
     let types = topic_names_and_types.get("/graph_test_topic_2").unwrap();
+    assert!(types.contains(&"test_msgs/msg/BasicTypes".to_string()));
 
+    let types = topic_names_and_types.get("/graph_test_topic_3").unwrap();
     assert!(types.contains(&"test_msgs/msg/Defaults".to_string()));
 
     Ok(())
@@ -113,8 +120,7 @@ fn check_topic_names_and_types(node: &Node) -> Result<(), RclrsError> {
 
 fn check_services(node: &Node) -> Result<(), RclrsError> {
     let check_names_and_types = |names_and_types: TopicNamesAndTypes| {
-        let types = names_and_types.get("/graph_test_topic_3").unwrap();
-
+        let types = names_and_types.get("/graph_test_topic_4").unwrap();
         assert!(types.contains(&"test_msgs/srv/Empty".to_string()));
     };
 
@@ -131,8 +137,7 @@ fn check_services(node: &Node) -> Result<(), RclrsError> {
 fn check_clients(node: &Node) -> Result<(), RclrsError> {
     let client_names_and_types =
         node.get_client_names_and_types_by_node(&node.name(), &node.namespace())?;
-    let types = client_names_and_types.get("/graph_test_topic_3").unwrap();
-
+    let types = client_names_and_types.get("/graph_test_topic_4").unwrap();
     assert!(types.contains(&"test_msgs/srv/Empty".to_string()));
 
     Ok(())
